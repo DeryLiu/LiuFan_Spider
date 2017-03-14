12e55a1b4ce66be9716e4b3cab859a0c114764b5
刘蘩 <liufan.dery@gmail.com>
2017-03-14 15:17:45 +0800

    20170314
    更新

---
+++ House/KuJiaLe/KuJiaLe_all_city_house.py
@@ -0,0 +1,149 @@
+from multiprocessing import Pool,Lock
+import requests
+from bs4 import BeautifulSoup
+import pymysql,random,time
+import urllib.request
+
+def get_proxies_ip():
+    # db = pymysql.connect("localhost","root","123456","Spider_Data",charset='utf8')
+    db = pymysql.connect("192.168.1.231","root","3jw9lketj0","ConstructionMaterials",charset='utf8')
+    cursor = db.cursor()
+    sql = "SELECT * FROM proxies_info;"
+    proxies_list = []
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            proxy_ip = row[1]
+            proxy_port = str(row[2])
+            proxies_list.append(proxy_ip+':'+proxy_port)
+    except:
+        db.rollback()
+    db.close()
+    proxies = {
+        'http':'http://'+random.choice(proxies_list)
+    }
+    return proxies
+
+def get_headers():
+    USER_AGENTS = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+    ]
+    return {
+        'User-Agent': random.choice(USER_AGENTS),
+        'Accept': '*/*',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+        # 'Connection': 'keep-alive',
+        'Host':'www.kujiale.com',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Referer':'http://www.kujiale.com/huxing/flash',
+        'X-Requested-With':'XMLHttpRequest',
+    }
+
+def makeMyOpener():
+    headers = get_headers()
+    proxies_list = get_proxies_ip()
+    # cj = http.cookiejar.CookieJar() # urllib.request.HTTPCookieProcessor(cj)
+    proxy_support = urllib.request.ProxyHandler(get_proxies_ip())
+    opener = urllib.request.build_opener(proxy_support,urllib.request.HTTPHandler)
+    header = []
+    for key, value in headers.items():
+        elem = (key, value)
+        header.append(elem)
+    opener.add_headers = header
+
+    urllib.request.install_opener(opener)
+    return opener
+
+def get_info(info_lst):
+    try:
+        basic_url = 'http://www.kujiale.com/api/openfps?query={city_code}-s{low_area}e{high_area}-{room_count}&start={start_num}&num=15'
+        over130_url = 'http://www.kujiale.com/api/openfps?query={city_code}-s{low_area}-{room_count}&start={start_num}&num=15'
+        room_counts_list = ['yijushi','erjushi','sanjushi','sijushi','wujushi']
+        areas_list = [0,50,80,100,130]
+        city_name = info_lst[0]
+        province_id = info_lst[1]
+        city_id = info_lst[2]
+        city_short = info_lst[3].strip()
+        for counts in range(5):
+            for room_counts in room_counts_list:
+                if counts == 4:
+                    url_list = [over130_url.format(city_code=city_short,low_area=130,room_count=room_counts,start_num=i*15) for i in range(20)]
+                else:
+                    url_list = [basic_url.format(city_code=city_short,low_area=areas_list[counts],high_area=areas_list[counts+1],room_count=room_counts,start_num=i*15) for i in range(20)]
+                for url in url_list:
+                    time.sleep(1)
+                    # data_request = requests.get(url,headers=get_headers(),proxies=get_proxies_ip())
+                    false = False
+                    true = True
+                    null = None
+                    # data_info = eval(data_request.text)
+                    opr = makeMyOpener()
+                    uop = opr.open(url, timeout = 30)
+                    data_info = eval(uop.read())
+
+                    if data_info['count'] != 0:
+                        for house in data_info['obsExFps']:
+                            lock.acquire()
+                            KuJiaLe_file.write(house['planCity']+'\t'+province_id+'\t'+city_id+'\t'+house['commName']+'\t'+house['specName']+'\t'+str(house['realArea'])+'\t'+str(house['cityId'])+'\t'+str(house['obsPlanId'])+'\t'+house['pics']+'\t'+str(house['obsCommId'])+'\n')
+                            KuJiaLe_file.flush()
+                            lock.release()
+    except Exception as e:
+        print(e)
+
+def info_handle():
+    global lock,KuJiaLe_file
+    lock = Lock()
+    KuJiaLe_file = open('./data/KuJiaLe_house_info.csv','w')
+    info_file = open('./data/KuJiaLe_city_info.csv','r')
+    info_list = []
+    for city_infos in info_file.readlines():
+        city_info = city_infos.split('\t')
+        city_name = city_info[0]
+        province_id = city_info[1]
+        city_id = city_info[2]
+        city_short = city_info[3]
+        if city_id != province_id:
+            info_list.append((city_name,province_id,city_id,city_short))
+
+    pool = Pool(20)
+    pool.map(get_info,info_list)
+    pool.close()
+    pool.join()
+    KuJiaLe_file.close()
+
+if __name__ == '__main__':
+    info_handle()
