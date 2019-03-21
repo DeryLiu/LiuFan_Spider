f348dae09e84ca9819ebeb955aac28cf0b1908a6
DeryLiu <liufan.dery@gmail.com>
2019-03-21 08:32:54 +0800

    删除

--- 购物类/Half_ebay/config.py
+++
@@ -1,6 +0,0 @@
-opt='Comic&GraphicNovels,http://books.products.half.ebay.com/comic-graphic-novels_W0QQcZ4QQcatZ218183'
-page=3000
-begin_num=''
-end_num='200'
-category_line=2
-
--- 购物类/Newegg/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm items.txt
-rm items_last.txt
-rm html/*
-rm except.txt
